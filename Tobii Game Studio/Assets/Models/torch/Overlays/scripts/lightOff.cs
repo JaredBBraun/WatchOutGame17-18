84681bc8d3d5b300d43e9f122e60e6deaa414c47
JaredBBraun <jaredbbraun@gmail.com>
2017-09-04 17:38:59 -0400

    added fourth quarter
    added fourth quarter (lab)

---
+++ Tobii Game Studio/Assets/Models/torch/Overlays/scripts/lightOff.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightOff : MonoBehaviour
+{
+    public GameObject light_off;
+    public GameObject flam;
+
+    private void OnTriggerExit()
+    {
+
+        light_off.SetActive(false);
+        flam.SetActive(false);
+    }
+
+}
---
+++ Tobii Game Studio/Assets/Models/torch/Overlays/scripts/triggerr_light.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerr_light : MonoBehaviour
+{
+
+	public GameObject light;
+    public GameObject flam;
+
+	private void OnTriggerEnter()
+	{
+		light.SetActive (true);
+        flam.SetActive(true);
+	}
+
+}
