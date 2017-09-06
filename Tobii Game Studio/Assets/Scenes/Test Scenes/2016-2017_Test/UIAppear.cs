1bfdc8d0b4a1fb3764e89be907716783a849854e
coleleforestier <coleleforestier@gmail.com>
2017-09-06 14:12:06 -0400

    added UI script to appear with text
    appears with text when player starts tutorial level

---
+++ Tobii Game Studio/Assets/Scenes/Test Scenes/2016-2017_Test/UIAppear.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIAppear : MonoBehaviour {
+
+
+    //[SerializeField] 
+    public Text tutText;
+
+    private void Start()
+    {
+        tutText.enabled = false;
+    }
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            tutText.enabled = true;
+        }  
+    }
+
+void OnTriggerExit(Collider other)
+{
+    if (other.CompareTag("Player"))
+    {
+        tutText.enabled = false;
+    }
+}
+}
