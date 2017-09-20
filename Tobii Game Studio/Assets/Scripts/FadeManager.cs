d98b2790792f228a3898b8e744f2e49102c9a7ba
KerolosSeleman <31482565+KerolosSeleman@users.noreply.github.com>
2017-09-20 13:45:20 -0400

    worked on fade in script.
    script does not work but wanted to save my work so yolo swag

---
+++ Tobii Game Studio/Assets/Scripts/FadeManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+
+    public static FadeManager instance { set; get; }
+
+    public Image fadeImage;
+    private bool isInTransition;
+    private float transition;
+    private bool isShowing;
+    private float duration;
+
+    private void Awake()
+    {
+        instance = this;
+
+    }
+    public void Fade(bool showing,float duration)
+    {
+        isShowing = showing;
+        isInTransition = true;
+        this.duration = duration;
+        transition = (isShowing) ? 0 : 1;
+    }
+
+    private void Update()
+
+
+    {
+
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        
+        string sceneName = currentScene.name;
+
+
+        if (sceneName == "TestScene")
+        {
+        Fade(true,1.25f);
+        }
+
+        //if (InputManager.BButton())
+   // {
+   //  Fade(false,3.0f);
+   // }
+
+        if (!isInTransition)
+            return;
+        transition += (isShowing) ? Time.deltaTime * (1 / duration) : -Time.deltaTime * (1 / duration);
+        fadeImage.color = Color.Lerp(new Color(1, 1, 1, 0), Color.black, transition);
+
+        if (transition > 1 || transition < 0)
+            isInTransition = false;
+    }
+}
