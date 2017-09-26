ca008cd94df99e32c42d6a382ccc3762c40d4f8e
SeanGunnery <seanw.gunnery@gmail.com>
2017-09-26 18:10:15 -0400

    Stitching and... Bleeding... a lot.. ow
    - Moved all of the scenes from the rest of our designers except for the kitchen area.
    - WO_Level1_01 has almost all of the rooms except for the kitchen

--- Tobii Game Studio/Assets/Standard Assets/Gaze Character Controllers/Sources/Scripts/gazeLook.cs
+++ Tobii Game Studio/Assets/Standard Assets/Gaze Character Controllers/Sources/Scripts/gazeLook.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-
 /// MouseLook rotates the transform based on the mouse delta.
 /// Minimum and Maximum values can be used to constrain the possible rotation
 
@@ -15,11 +14,9 @@ using System.Collections;
 /// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
 /// - Add a MouseLook script to the camera.
 ///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
-//[AddComponentMenu("Camera-Control/Mouse Look")]
-public class gazeLook : MonoBehaviour {
-    
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MouseLook : MonoBehaviour {
 
-    public GameObject flashlight;
 	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
 	public RotationAxes axes = RotationAxes.MouseXAndY;
 	public float sensitivityX = 15F;
@@ -33,34 +30,34 @@ public class gazeLook : MonoBehaviour {
 
 	float rotationY = 0F;
 
-    void Update ()
+	void Update ()
 	{
-        if (axes == RotationAxes.MouseXAndY)
-        {
-        	float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-        	
-        	rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        	rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-        	
-        	transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-        	transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
-        else
-        {
-        	rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        	rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-        	transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        }
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
 	}
 	
 	void Start ()
 	{
-        //myLookAt = new MyLookAt()
-        // Make the rigid body not change rotation
-        if (GetComponent<Rigidbody>())
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
 			GetComponent<Rigidbody>().freezeRotation = true;
 	}
 }
