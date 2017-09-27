62480750186ec48d9f40b56c3163ed50101e7d8b
JaredBBraun <jaredbbraun@gmail.com>
2017-09-27 13:33:54 -0400

    Added Kitchen to Master

---
+++ Tobii Game Studio/Assets/Clean/FlickeringLight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+
+	Light testLight;
+	public float minWaitTime;
+	public float maxWaitTime;
+
+	void Start () {
+		testLight = GetComponent<Light>();
+		StartCoroutine(Flashing());
+	}
+
+	IEnumerator Flashing ()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Random.Range(minWaitTime,maxWaitTime));
+			testLight.enabled = ! testLight.enabled;
+
+		}
+	}
+}
