8dd0c09923f559a7bd9b742878ddc079f08195de
JaredBBraun <jaredbbraun@gmail.com>
2017-09-27 02:44:48 -0400

    Made new flashlight stuff
    cookie and script

---
+++ Tobii Game Studio/Assets/Scripts/FashLightTypes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FLMODE
+{
+    STANDARD,
+    SCAN,
+    SCARE,
+}
+
+
+public class FashLightTypes : MonoBehaviour {
+    private Light flashlight;
+    public Texture scan;
+
+    [SerializeField]
+    FLMODE mode = FLMODE.STANDARD;
+	// Use this for initialization
+	void Start () {
+        flashlight = GetComponent<Light>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mode == FLMODE.SCAN)
+        {
+            flashlight.cookie = scan;
+            //see things through walls at a distance
+            //might be able to have a "wall hack"
+        }
+        else if (mode == FLMODE.STANDARD)
+        {
+            flashlight.cookie = null;
+        }
+	}
+}
